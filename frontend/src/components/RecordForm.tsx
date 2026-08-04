6db65ba36680e06e018e546af6211d48196330dd
Alberto Chevez <146797435+AbrtoSW@users.noreply.github.com>
2026-08-04 16:00:17 -0400

    update recordform.tsx
    removed owner field

--- frontend/src/components/RecordForm.tsx
+++ frontend/src/components/RecordForm.tsx
@@ -42,13 +42,7 @@ function RecordForm({ values, errors, isSubmitting, onChange, onSubmit, classNam
         {errors.name ? <small id="name-error">{errors.name}</small> : null}
       </label>
       
-      <label className='field'>
-        Owner
-        <input
-          value={values.owner}
-          onChange={(event) => onChange("owner", event.target.value)}
-        />
-      </label>
+      
 
       <label className="field">
         <span>Email</span>
